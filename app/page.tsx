efe5b5beb3298e7ff2102356aff65f769ce1c34b
IanFSA <ian@thescarlettcompany.co.za>
2026-07-09 14:49:15 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import CueBoard from "@/app/components/CueBoard";
-import { isAuthenticated } from "@/app/lib/auth";
-
-export default async function Home() {
-  return <CueBoard initialAuthenticated={await isAuthenticated()} />;
-}
